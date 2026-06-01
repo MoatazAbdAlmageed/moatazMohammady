a822832ce6306d97bc24413411f4bd9fe7688e3a
Moataz Mohammady <moataz.mohammady@gmail.com>
2026-06-01 11:01:30 +0300

    feat: configure static site export and add GitHub Actions deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = 'moatazMohammady';
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? `/${repoName}` : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
